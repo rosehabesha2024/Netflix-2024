5730251c622e52646a14b15b00defc8a3750b04d
rosehabesha2024 <rosehabesha2020@gmail.com>
2025-02-16 00:03:20 +0300

    I have done the header banner Rows and Footer

---
+++ src/Components/Rows/RowList/RowList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Row from "../Row/Row";
+import requests from "../../../Utils/requests";
+
+function RowList() {
+  return (
+    <>
+      <Row
+        title="NetflixOriginals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="TV Shows" fetchUrl={requests.fetchTvShow} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </>
+  );
+}
+
+export default RowList;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from "../Components/Header/Header"
+import Footer from "../Components/Footer/Footer"
+import RowList from "../Components/Rows/RowList/RowList"
+import Banner from "../Components/Banner/Banner"
+
+
+function Home() {
+  return (
+    <>
+    <Header />
+    < Banner />
+    < RowList />
+    <Footer />
+
+
+
+    </>
+  )
+}
+
+export default Home
